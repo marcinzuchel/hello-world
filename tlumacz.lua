5d57f553b78bf64e91be984e124d1178015654e0
Marcin Żuchelkowski <marcinzuchel@users.noreply.github.com>
2017-05-07 22:15:35 +0200

    Create tlumacz.lua

---
+++ tlumacz.lua
@@ -0,0 +1,16 @@
+#!/usr/bin/lua
+
+i = 1;
+e = {};
+f = " ";
+repeat
+
+    a = io.read(1)
+    b = io.read(1)
+    c = io.read(1)
+    d = a..b
+    e[i] = (string.char(tonumber(d, 16)))
+
+    io.write(e[i])
+    i = i + 1
+until c == "/n"
